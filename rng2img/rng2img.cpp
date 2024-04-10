6f401f951c28a055657beb32b9611cb5e8854cd5
Mark Jarzynski <mark.jarzynski@gmail.com>
2024-04-10 16:54:08 -0500

    Updated rng2img.cpp to print out to a file. Added commented out code to write to a file.

--- rng2img/rng2img.cpp
+++ rng2img/rng2img.cpp
@@ -5,18 +5,33 @@
 
 int main(int argc, char** argv)
 {
-    printf("hello\n");
+    uint8_t (*pixels)[3] = new uint8_t[SIZE*SIZE][3];
+    
 
+    uint8_t (*pixel)[3] = pixels;
     for (int i = 0; i < SIZE; i++)
     {
         for (int j = 0; j < SIZE; j++)
         {
             uint3 v = pcg3d(uint3(i, j, 0u));
-            printf("%d %d %d\n", v.x, v.y, v.z);
-            
-            
+
+            (*pixel)[0] = uint8_t(v.x % 255);
+            (*pixel)[1] = uint8_t(v.y % 255);
+            (*pixel)[2] = uint8_t(v.z % 255);
+
+            pixel++;
         }
     }
+
+    /*
+    FILE* f = fopen("out.ppm", "wb");
+    fprintf(f, "P6\n%d %d\n%d\n", SIZE, SIZE, 255);
+    fwrite(pixels, 1, SIZE*SIZE*3, f);
+    fclose(f);
+    */
+    
+    fprintf(stdout, "P6\n%d %d\n%d\n", SIZE, SIZE, 255);
+    fwrite(pixels, 1, SIZE*SIZE*3, stdout);
 
     return 0;
 }

15e9e8761dbae67302b7a2dd217fff33b61cf2db
Mark Jarzynski <mark.jarzynski@gmail.com>
2024-04-15 23:42:56 -0400

    Moved .h to include directory. Updated Makefile. Updated rng2img.cpp to include all of the interger hashes in random.h

--- rng2img/rng2img.cpp
+++ rng2img/rng2img.cpp
@@ -63,31 +63,88 @@ uint3 hash2rgb(char* hashname, uint x, uint y)
 {
     if (false) {}
 
-#define ELSEIF11(NAME, HASH)                \
-    else if (strcmp(hashname, NAME) == 0)   \
+#define ELSEIF11(HASH)                      \
+    else if (strcmp(hashname, #HASH "_linear") == 0) \
     {                                       \
         uint h = HASH(seed(uint2(x,y)));    \
         return uint3(h,h,h);                \
+    }                                       \
+    else if (strcmp(hashname, #HASH "_nested") == 0) \
+    {                                       \
+        uint h = HASH(HASH(x) + y);         \
+        return uint3(h,h,h);                \
     }
 
-#define ELSEIF21(NAME, HASH)                \
-    else if (strcmp(hashname, NAME) == 0)   \
+
+#define ELSEIF12(HASH)                      \
+    else if (strcmp(hashname, #HASH) == 0)  \
     {                                       \
         uint h = HASH(uint2(x,y));          \
         return uint3(h,h,h);                \
     }
 
-#define ELSEIF33(NAME, HASH)                \
-    else if (strcmp(hashname, NAME) == 0)   \
+#define ELSEIF33(HASH)                      \
+    else if (strcmp(hashname, #HASH) == 0)  \
     {                                       \
         uint3 h = HASH(uint3(x, y, 0u));    \
         return h;                           \
     }
 
+#define ELSEIF44(HASH)                      \
+    else if (strcmp(hashname, #HASH) == 0)  \
+    {                                       \
+        uint4 h = HASH(uint4(x, y, 0u, 0u));\
+        return uint3(h.x, h.y, h.z);        \
+    }
 
-    ELSEIF21("city", city)
-    ELSEIF11("lcg", lcg)
-    ELSEIF33("pcg3d", pcg3d)
+#define ELSEIF14(HASH)                      \
+    else if (strcmp(hashname, #HASH) == 0)  \
+    {                                       \
+        uint h = HASH(uint4(x, y, 0u, 0u)); \
+        return uint3(h, h, h);              \
+    }
+
+
+    ELSEIF12(city)
+    ELSEIF11(esgtsa)
+    ELSEIF11(iqint1)
+    ELSEIF33(iqint2)
+    ELSEIF12(iqint3)
+    ELSEIF11(jkiss32)
+    ELSEIF11(lcg)
+    ELSEIF44(md5)
+    ELSEIF12(murmur3)
+    ELSEIF33(pcg3d)
+    ELSEIF44(pcg4d)
+    ELSEIF11(ranlim32)
+    ELSEIF12(superfast)
+    
+    else if (strcmp(hashname, "tea2") == 0)
+    {
+        uint2 h = tea(2, uint2(x,y));
+        return uint3(h.x, h.y, 0u);
+    }
+    else if (strcmp(hashname, "tea3") == 0)
+    {
+        uint2 h = tea(3, uint2(x,y));
+        return uint3(h.x, h.y, 0u);
+    }
+    else if (strcmp(hashname, "tea4") == 0)
+    {
+        uint2 h = tea(4, uint2(x,y));
+        return uint3(h.x, h.y, 0u);
+    }
+    else if (strcmp(hashname, "tea5") == 0)
+    {
+        uint2 h = tea(5, uint2(x,y));
+        return uint3(h.x, h.y, 0u);
+    }
+
+    ELSEIF11(wang)
+    ELSEIF44(xorshift128)
+    ELSEIF11(xorshift32)
+    ELSEIF12(xxhash32)
+    ELSEIF14(hybridtaus)
 
     else
     {

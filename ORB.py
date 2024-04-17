81cf4a1ad3df6f39f1900f2f45d872a5b6fa50c4
Glimmering-Stallion <130874889+Glimmering-Stallion@users.noreply.github.com>
2024-04-16 22:53:30 -0400

    File for ORB algorithm
    Used a filtered list with conditions to apply Lowe's ratio test and a spatial distance threshold to refine the keypoint matches.
    
    - The ratio test makes sure that the second match is significantly closer than the third match, which should reduce the likelihood of false positives.
    - Adding a minimum pixel distance criteria (min_distance = [some value]) to filter out trivial self-matches where keypoints are coincident. This helps with our feature detection goals because we are comparing images against themselves.

--- ORB.py
+++ ORB.py
@@ -1,39 +1,39 @@
-import sys
 import cv2
+import numpy as np
 import matplotlib.pyplot as plt
 
 def ORB(image):
-    img1 = image.copy()
-    img2 = image.copy()
-    
-    gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
-    gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
-    
-    orb = cv2.ORB_create()
-    
-    kp1, des1 = orb.detectAndCompute(gray1,None)
-    kp2, des2 = orb.detectAndCompute(gray2,None)
-    
-    bf = cv2.BFMatcher()
-    
-    matches = bf.knnMatch(des1, des2, k=2)
-    matches = [y for (x,y) in matches]
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    img3 = cv2.drawMatches(img1, kp1, img2, kp2, matches[:50], img2, flags=2) 
+    detector = cv2.ORB_create()
+    keypoints, descriptors = detector.detectAndCompute(gray, None)
 
-    return img3
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=False)
+    matches = bf.knnMatch(descriptors, descriptors, k=3)
 
-    plt.imshow(image),plt.show()
+    filtered_matches = []
+    thres_ratio = 0.75
+    min_distance = 50  # set minimum pixel distance to filter trivial self-matches
 
-def usage():
-    print("Usage: ./ORB.py <image>")
+    filtered_matches = [
+        n for _, m, n in matches                            # ignore first match in knn as it is always a self-match
+        if (m.distance / n.distance) < thres_ratio and      # Lowe's ratio test to filter matches
+        np.linalg.norm(np.array(keypoints[n.queryIdx].pt) - np.array(keypoints[n.trainIdx].pt)) >= min_distance
+    ]
+
+    # debugging
+    print("Total matches found:", len(matches))
+    print("Number of keypoints:", len(keypoints))
+    print("Number of refined matches:", len(filtered_matches))
+
+    matched_image = cv2.drawMatches(image, keypoints, image, keypoints, filtered_matches[:30], None, flags=2)
+    return matched_image
 
 if __name__ == "__main__":
+    image = cv2.imread('./rng2img/hybridtaus.png')
 
-    if len(sys.argv) != 2:
-        usage()
-        exit()
-
-    image = cv2.imread(sys.argv[1])
-    image = ORB(image)
-    cv2.imwrite('ORB_out.png', image) 
+    if image is None:
+        print("Error: Image did not load.")
+    else:
+        image = ORB(image)
+        plt.imshow(image),plt.show()

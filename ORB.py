54a90af6c2ac13be6593fb0518d2d03bd0bef01a
Mark Jarzynski <mark.jarzynski@gmail.com>
2024-05-05 02:45:05 -0400

    Added util.py, which has filtering and draw functions. Updated ORB.py to use the fucntions in util.py and to continue finding matches until only 2 matches are found.

--- ORB.py
+++ ORB.py
@@ -1,8 +1,11 @@
+#!/usr/bin/env python
+import sys
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
 from RANSAC import ransac
+from util import *
 
 def ORB(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -12,30 +15,52 @@ def ORB(image):
 
     bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=False)
     matches = bf.knnMatch(descriptors, descriptors, k=2)
+    matches = [match for (_, match) in matches]
 
-    filtered_matches = []
-    min_distance = 70  # set minimum pixel distance to filter trivial self-matches
-
-    filtered_matches = [
-        match for (_, match) in matches if # ignore first match in knn as it is always a self-match
-        np.linalg.norm(np.array(keypoints[match.queryIdx].pt) - np.array(keypoints[match.trainIdx].pt)) >= min_distance
-    ]
+    filtered_matches = filter_distance(matches, keypoints, 5)
 
     _, _, filtered_matches = ransac(filtered_matches, keypoints, keypoints)
+
+    group = [filtered_matches]
+    m = matches
+
+    while True:
+
+        m = filter_matches(m, filtered_matches)
+        _, _, filtered_matches = ransac(m, keypoints, keypoints)
+
+        if len(filtered_matches) > 2:
+            group.append(filtered_matches)
+        else:
+            break
 
     # debugging
     print("Total matches found:", len(matches))
     print("Number of keypoints:", len(keypoints))
-    print("Number of refined matches:", len(filtered_matches))
+    #print("Number of refined matches:", len(filtered_matches))
+    print("Number of groups:", len(group))
+    print("Group sizes:", ",".join([str(len(i)) for i in group]))
 
-    matched_image = cv2.drawMatches(image, keypoints, image, keypoints, filtered_matches[:30], None, flags=2)
+    #matched_image = cv2.drawMatches(image, keypoints, image, keypoints, filtered_matches[:30], None, flags=2)
+
+    matched_image = draw_matches(image, group, keypoints)
+
     return matched_image
 
+def usage(program):
+    print(f"Usage: {program} <image>")
+
 if __name__ == "__main__":
-    image = cv2.imread('./rng2img/Hash Images/esgtsa_linear.png')
+
+    if len(sys.argv) != 2:
+        usage(sys.argv[0])
+        exit()
+
+    image = cv2.imread(sys.argv[1])
 
     if image is None:
         print("Error: Image did not load.")
-    else:
-        image = ORB(image)
-        plt.imshow(image),plt.show()
+        exit()
+
+    image = ORB(image)
+    plt.imshow(image),plt.show()
